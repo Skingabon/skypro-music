112d32763a90b117348252a33e58a4b06a1d0480
skingabon <reclama.katkov@gmail.com>
2024-01-05 13:23:11 +0300

    Decomposition app (7 component)

--- src/components/SideBar/sideBar.jsx
+++ src/components/SideBar/sideBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./sideBar.css";
+import { SideBarPlayList } from "../SideBarPlayList/SideBarPlayList";
 
 
 export function SideBar() {
@@ -14,37 +15,7 @@ export function SideBar() {
                 </svg>
               </div>
             </div>
-            <div className="sidebar__block">
-              <div className="sidebar__list">
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist01.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist02.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist03.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
+            <SideBarPlayList />
           </div>
     );
 }
