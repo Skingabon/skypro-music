e24ad3bbc29909c699186a358e611642cf3af886
Your Name <you@example.com>
2024-01-17 18:09:23 +0300

    filter pop-up done

--- src/components/CenterBlockFilter/CenterBlockFilter.jsx
+++ src/components/CenterBlockFilter/CenterBlockFilter.jsx
@@ -10,82 +10,90 @@ import "./centerBlockFilter.css";
 //элементы в списке должны юыть интерактивные, но без логики кликабельности
 //список треков и список исполнителей хранить в массиве!!! чтобы в списке категорий менялись данные в зависимости от выбранной категории
 
-
 const authorData = [
-    "Nero",
-    "Dynoro",
-    "Ali Bakgor",
-    "Стоункат",
-    "Calvin Harris",
-    "Tom Boxer",
-    "Jaded",
-    "Blue Foundation"
-]
+  "Nero",
+  "Dynoro",
+  "Ali Bakgor",
+  "Стоункат",
+  "Calvin Harris",
+  "Tom Boxer",
+  "Jaded",
+  "Blue Foundation",
+];
 
-const yearData = [
-    2017,
-    2018,
-    2019,
-    2020,
-    2021,
-    2022,
-    2023,
-    2024
-]
+const yearData = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
 
-const genreData = [
-    'Elektro',
-    'Pop',
-    'Trance',
-    'Folk',
-    'Shanson'
-]
+const genreData = ["Elektro", "Pop", "Trance", "Folk", "Shanson"];
 
 export function CenterBlockFilter() {
-    const [activeFilter, setActiveFilter] = useState(null)
+  const [activeFilter, setActiveFilter] = useState(null);
 
-const handleActiveFilter = (nameFilter) => {
+  const handleActiveFilter = (nameFilter) => {
     if (nameFilter === activeFilter) {
-        setActiveFilter(null) 
-        return
-         }
-         setActiveFilter(nameFilter) 
- 
-}
+      setActiveFilter(null);
+      return;
+    }
+    setActiveFilter(nameFilter);
+  };
 
-    return (
-        <div className="centerblock__filter filter">
-            <div className="filter__title">Искать по:</div>
+  return (
+    <div className="centerblock__filter filter">
+      <div className="filter__title">Искать по:</div>
 
-            <div className="filter-container">
-
-                <div className="filter__button button-author _btn-text" onClick={() => handleActiveFilter("author")}>
-                    исполнителю
-                </div>
-
-                {activeFilter === "author" && (
-                    <ul className="filter-list">
-                        {authorData.map((elem, index) => (
-
-                            <li key={index} className="filter-list__item">
-
-                                {elem}
-
-                            </li>
-                        ))}
-
-                    </ul>
-                )}
-
-            </div>
-
-
-
-
-            <div className="filter__button button-year _btn-text">
-                году выпуска
-            </div>
-            <div className="filter__button button-genre _btn-text">жанру</div>
+      <div className="filter-container__author">
+        <div
+          className="filter__button button-author _btn-text"
+          onClick={() => handleActiveFilter("author")}
+        >
+          исполнителю
         </div>
-    )
+
+        {activeFilter === "author" && (
+          <ul className="filter-list__author">
+            {authorData.map((elem, index) => (
+              <li key={index} className="filter-list__item">
+                {elem}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <div className="filter-container__year">
+        <div
+          className="filter__button button-year _btn-text"
+          onClick={() => handleActiveFilter("year")}
+        >
+          году выпуска
+        </div>
+        {activeFilter === "year" && (
+          <ul className="filter-list__year">
+            {yearData.map((elem, index) => (
+              <li key={index} className="filter-list__item">
+                {elem}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <div className="filter-container__genre">
+        <div
+          className="filter__button button-genre _btn-text"
+          onClick={() => handleActiveFilter("genre")}
+        >
+          жанру
+        </div>
+        {activeFilter === "genre" && (
+          <ul className="filter-list__genre">
+            {genreData.map((elem, index) => (
+              <li key={index} className="filter-list__item">
+                {elem}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/components/SideBarPlayList/SideBarPlayList.jsx
+++ src/components/SideBarPlayList/SideBarPlayList.jsx
@@ -1,38 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sideBarPlayList.css";
 
 export function SideBarPlayList() {
-    return (
-        <div className="sidebar__block">
-            <div className="sidebar__list">
-                <div className="sidebar__item">
-                    <a className="sidebar__link" href="#">
-                        <img
-                            className="sidebar__img"
-                            src="img/playlist01.png"
-                            alt="day's playlist"
-                        />
-                    </a>
-                </div>
-                <div className="sidebar__item">
-                    <a className="sidebar__link" href="#">
-                        <img
-                            className="sidebar__img"
-                            src="img/playlist02.png"
-                            alt="day's playlist"
-                        />
-                    </a>
-                </div>
-                <div className="sidebar__item">
-                    <a className="sidebar__link" href="#">
-                        <img
-                            className="sidebar__img"
-                            src="img/playlist03.png"
-                            alt="day's playlist"
-                        />
-                    </a>
-                </div>
+  const [isLoading, setIsLoading] = useState(true);
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 5000);
+
+  return (
+    <div className="sidebar__block">
+      {isLoading ? (
+        <img src="img/Category.svg" alt="" />
+      ) : (
+        <>
+          <div className="sidebar__list">
+            <div className="sidebar__item">
+              <a className="sidebar__link" href="#">
+                <img
+                  className="sidebar__img"
+                  src="img/playlist01.png"
+                  alt="day's playlist"
+                />
+              </a>
             </div>
-        </div>
-    );
+            <div className="sidebar__item">
+              <a className="sidebar__link" href="#">
+                <img
+                  className="sidebar__img"
+                  src="img/playlist02.png"
+                  alt="day's playlist"
+                />
+              </a>
+            </div>
+            <div className="sidebar__item">
+              <a className="sidebar__link" href="#">
+                <img
+                  className="sidebar__img"
+                  src="img/playlist03.png"
+                  alt="day's playlist"
+                />
+              </a>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
