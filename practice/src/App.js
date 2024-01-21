71776b30f7123c0a3d935896b73ef54f071c540a
skingabon <reclama.katkov@gmail.com>
2024-01-21 19:18:12 +0300

    create app for test

---
+++ practice/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+const todos = {
+  todos: [
+    {
+    text: "посмотреть новый урок",
+    id: 15,
+    created_at: "2023-07-05T15:14:15.080Z",
+    user: null,
+
+  },
+  {
+    text: "сдать домашку",
+    id: 16,
+    created_at: "2023-08-05T15:14:15.080Z",
+    user: null,
+
+  },
+  {
+    text: "попрактиковаться",
+    id: 17,
+    created_at: "2023-09-05T15:14:15.080Z",
+    user: null,
+
+  },
+ ],
+};
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
