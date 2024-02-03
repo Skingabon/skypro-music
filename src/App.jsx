e045c6b558c8febc1bb99b780733f0a17c0915b8
skingabon <reclama.katkov@gmail.com>
2024-02-03 14:25:25 +0300

    Roauting Main Page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,16 @@
-// import "./App.css";
+import React from "react";
+import { AppRoutes } from "./AppRoutes";
+import { GlobalStyle } from "./app.styled"
 
-import React, { useEffect, useState } from "react";
-import { PlayerBar } from "./components/PlayerBar/PlayerBar";
-import { SideBar } from "./components/SideBar/SideBar";
-import { NavMenu } from "./components/NavMenu/NavMenu";
-import { CenterBlock } from "./components/CenterBlock/CenterBlock";
-import { GlobalStyle } from "./app.styled";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  //   //1. При использовании setTimeout внутри useEffect с пустым массивом зависимостей ([]), мы гарантируем, что setTimeout будет запущен только один раз после монтирования компонента. Это предотвращает многократное создание таймеров при обновлении компонента.
-
-  // 2. Помещение setTimeout в useEffect также помогает в управлении жизненным циклом таймера. Когда компонент размонтируется, useEffect выполнит очистку (clean-up) и отменит таймер, чтобы избежать утечек памяти.
-
-  // 3. Использование setTimeout внутри useEffect делает код более читаемым и предсказуемым, так как это стандартная практика для выполнения побочных эффектов в React.
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-  }, []);
-
+ 
   return (
     <>
-      <GlobalStyle />
-      <div className="wrapper">
-        <div className="container">
-          <main className="main">
-            <NavMenu />
-            <CenterBlock />
-            <SideBar />
-          </main>
-          <PlayerBar isLoading={isLoading} />
-          <footer className="footer"></footer>
-        </div>
-      </div>
+    <GlobalStyle/>
+    <AppRoutes />
     </>
+
   );
 }
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { NavMenu } from "../../components/NavMenu/NavMenu";
+import { CenterBlock } from "../../components/CenterBlock/CenterBlock";
+import { SideBar } from "../../components/SideBar/SideBar";
+import { PlayerBar } from "../../components/PlayerBar/PlayerBar";
+
+
 
 export const Main = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    //   //1. При использовании setTimeout внутри useEffect с пустым массивом зависимостей ([]), мы гарантируем, что setTimeout будет запущен только один раз после монтирования компонента. Это предотвращает многократное создание таймеров при обновлении компонента.
+  
+    // 2. Помещение setTimeout в useEffect также помогает в управлении жизненным циклом таймера. Когда компонент размонтируется, useEffect выполнит очистку (clean-up) и отменит таймер, чтобы избежать утечек памяти.
+  
+    // 3. Использование setTimeout внутри useEffect делает код более читаемым и предсказуемым, так как это стандартная практика для выполнения побочных эффектов в React.
+  
+    useEffect(() => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 5000);
+    }, []);
+  
     return (
-        <div>
-            <h1>Main</h1>
+      <>
+        {/* <GlobalStyle/> */}
+        <div className="wrapper">
+          <div className="container">
+            <main className="main">
+              <NavMenu />
+              <CenterBlock />
+              <SideBar />
+            </main>
+            <PlayerBar isLoading={isLoading} />
+            <footer className="footer"></footer>
+          </div>
         </div>
-    )
-}
+      </>
+    );
+};
