50e0eb12b96b992c05a6f336e13dc3d1ab3e1155
skingabon <reclama.katkov@gmail.com>
2024-03-04 13:16:08 +0300

    HW7 Done

--- src/AppRoutes/AppRoutes.jsx
+++ src/AppRoutes/AppRoutes.jsx
@@ -6,18 +6,18 @@ import { Category } from "../pages/Category/Category";
 import { ProtectedRoute } from "./ProtectedRoutes";
 import { Main } from "../pages/Main/Main";
 import { AuthPage } from "../pages/Auth/AuthPage";
-import { useTrackContext } from "../context/track";
+// import { useTrackContext } from "../context/track";
 
 export const AppRoutes = () => {
   
-  const {user} = useTrackContext()
+  // const {user} = useTrackContext()
 
 
 
 
   return (
     <Routes>
-      <Route element={<ProtectedRoute isAllowed={user} />}>
+      <Route element={<ProtectedRoute isAllowed={true} />}>
         <Route
           path="/"
           element={
@@ -47,7 +47,7 @@ export const AppRoutes = () => {
         <Route path="*" element={<NotFound />} />
       </Route>
 
-      <Route path="/login" element={<AuthPage isLoginMode={true} isAllowed={true}/>} />
+      <Route path="/login" element={<AuthPage isLoginMode={true} />} />
       <Route path="/registration" element={<AuthPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
