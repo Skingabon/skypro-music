f50acaf8b966cfcf452cccee673319cbcc0c2a05
skingabon <reclama.katkov@gmail.com>
2024-04-10 22:28:50 +0300

    HW10 От протухания токена

--- src/Api/tracksApi.js
+++ src/Api/tracksApi.js
@@ -1,51 +1,85 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const baseQueryRefresh = async (args, api, extraOptions) => {
+  const baseQuery = fetchBaseQuery({
+    baseUrl: "https://skypro-music-api.skyeng.tech",
+    prepareHeaders: (headers) => {
+      const token = JSON.parse(localStorage.getItem("user")).res.access
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`)
+      }
+      return headers
+    }
+  })
+  const refresh = JSON.parse(localStorage.getItem("user")).res.refresh
+  //console.log(refresh);
+  if (!refresh) {
+    // сделать выход из приложения
+  }
+  const result = await baseQuery(args, api, extraOptions)
+  if (result?.error?.status === 401) {
+    const refreshToken = await baseQuery({
+      url: "/user/token/refresh/",
+      method: "POST",
+      body: {
+        refresh
+      }
+    }, api, extraOptions)
+    if (!refreshToken.data.access) {
+      // сделать выход из приложения
+      return
+    }
+    const user = JSON.parse(localStorage.getItem("user"))
+    localStorage.setItem("user", JSON.stringify({
+      ...user, res: {
+        access: refreshToken.data.access,
+        refresh: user.res.refresh
+      }
+    }))
+    const retryResult = await baseQuery(args, api, extraOptions)
+    return retryResult
+  }
+
+  return result
+}
+
+
 
 export const tracksApi = createApi({
-    reducerPath: "tracksApi",
-    tagTypes: ["LIKE"],
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://skypro-music-api.skyeng.tech/catalog",
-    }),
-    endpoints: (builder) => ({
-      getTracks: builder.query({
-        query: () => ({
-            url: "/track/all/",
-            method: "GET",
-            // headers: 
-        }),
-        providesTags: ["LIKE"]
+  reducerPath: "tracksApi",
+  tagTypes: ["LIKE"],
+  baseQuery: baseQueryRefresh,
+  endpoints: (builder) => ({
+    getTracks: builder.query({
+      query: () => ({
+        url: "/catalog/track/all/",
+        method: "GET",
+        // headers: 
+      }),
+      providesTags: ["LIKE"]
 
+    }),
+    getFavoriteTracks: builder.query({
+      query: () => ({
+        url: "/catalog/track/favorite/all/",
+        method: "GET",
+      })
+    }),
+    addFavoriteTrack: builder.mutation({
+      query: ({ id }) => ({
+        url: `/catalog/track/${id}/favorite/`,
+        method: "POST",
       }),
-      getFavoriteTracks: builder.query({
-        query: () => ({
-            url: "/track/favorite/all/",
-            method: "GET",
-            headers: {
-                authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).res.access}`
-            }
-        })
+      invalidatesTags: ["LIKE"]
+    }),
+    delFavoriteTrack: builder.mutation({
+      query: ({ id }) => ({
+        url: `/catalog/track/${id}/favorite/`,
+        method: "DELETE",
       }),
-      addFavoriteTrack: builder.mutation({
-        query: ({ id }) => ({
-            url: `/track/${id}/favorite/`,
-            method: "POST",
-            headers: {
-                authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).res.access}`
-            }
-        }),
-        invalidatesTags: ["LIKE"]
-      }),
-      delFavoriteTrack: builder.mutation({
-        query: ({ id }) => ({
-            url: `/track/${id}/favorite/`,
-            method: "DELETE",
-            headers: {
-                authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).res.access}`
-            }
-        }),
-        invalidatesTags: ["LIKE"]
-      }),
-    })
+      invalidatesTags: ["LIKE"]
+    }),
+  })
 })
 
 
